4991054268e410ebd89d414c5a01d4ad0259da91
strixqlater <strixqlater@gmail.com>
2020-05-20 22:20:24 +0900

    add MemberController

---
+++ KM_AF_SHOP/src/main/java/me/cloverclub/controller/MemberController.java
@@ -0,0 +1,28 @@
+package me.cloverclub.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import lombok.AllArgsConstructor;
+import lombok.extern.log4j.Log4j;
+
+@Controller
+@Log4j
+@AllArgsConstructor
+public class MemberController {
+	// private MemberService service;
+	
+	@GetMapping("/login")
+	public String getLogin() {
+		log.info("getLogin()");
+		
+		return "login";
+	}
+	
+	@GetMapping("/join")
+	public String getJoin() {
+		log.info("getJoin()");
+		
+		return "join";
+	}
+}
