354672049eef80fb231a29e1ce7a018e689c8bb7
strixqlater <strixqlater@gmail.com>
2020-05-20 22:16:22 +0900

    update HomeController

--- KM_AF_SHOP/src/main/java/me/cloverclub/controller/HomeController.java
+++ KM_AF_SHOP/src/main/java/me/cloverclub/controller/HomeController.java
@@ -1,22 +1,19 @@
 package me.cloverclub.controller;
 
-import java.util.Locale;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import lombok.AllArgsConstructor;
+import lombok.extern.log4j.Log4j;
+
 @Controller
+@Log4j
+@AllArgsConstructor
 public class HomeController {
-	
-	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
-	
 	@GetMapping("/")
-	public String home(Locale locale, Model model) {
+	public String getIndex() {
+		log.info("getIndex()");
 		
 		return "index";
-	}
-	
+	}	
 }
