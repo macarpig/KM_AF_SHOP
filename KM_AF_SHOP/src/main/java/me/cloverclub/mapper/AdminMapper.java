2063a0d23c5739483cd01378142e5b5d9e5d12f2
strixqlater <strixqlater@gmail.com>
2020-05-27 23:06:27 +0900

    re-add AdminController/Mapper/Service

---
+++ KM_AF_SHOP/src/main/java/me/cloverclub/mapper/AdminMapper.java
@@ -0,0 +1,5 @@
+package me.cloverclub.mapper;
+
+public interface AdminMapper {
+
+}
---
+++ KM_AF_SHOP/src/main/java/me/cloverclub/service/AdminServiceImpl.java
@@ -0,0 +1,13 @@
+package me.cloverclub.service;
+
+import org.springframework.stereotype.Service;
+
+import lombok.AllArgsConstructor;
+import lombok.extern.log4j.Log4j;
+
+@Log4j
+@Service
+@AllArgsConstructor
+public class AdminServiceImpl implements AdminService {
+
+}
