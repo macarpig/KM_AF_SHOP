0bfa126fc65d6721ee8703f7b23efc521450093e
strixqlater <strixqlater@gmail.com>
2020-06-02 15:43:41 +0900

    add category function at admin goods add pages

--- KM_AF_SHOP/src/main/java/me/cloverclub/controller/AdminController.java
+++ KM_AF_SHOP/src/main/java/me/cloverclub/controller/AdminController.java
@@ -1,19 +1,26 @@
 package me.cloverclub.controller;
 
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import lombok.AllArgsConstructor;
 import lombok.extern.log4j.Log4j;
 import me.cloverclub.service.AdminService;
+import me.cloverclub.service.CategoryService;
+import me.cloverclub.vo.CategoryVO;
+import net.sf.json.JSONArray;
 
 @Controller
 @Log4j
 @AllArgsConstructor
 @RequestMapping("/admin")
 public class AdminController {
-	private AdminService service;
+	private AdminService a_service;
+	private CategoryService c_service;
 	
 	@GetMapping("/index")
 	public String getIndex() {
@@ -23,8 +30,12 @@ public class AdminController {
 	}
 	
 	@GetMapping("/goods/add")
-	public String getGoodsAdd() {
+	public String getGoodsAdd(Model model) throws Exception {
 		log.info("AdminController: getGoodsAdd()");
+		
+		List<CategoryVO> category = null;
+		category = c_service.category();
+		model.addAttribute("category", JSONArray.fromObject(category));
 		
 		return "/admin/add";
 	}
