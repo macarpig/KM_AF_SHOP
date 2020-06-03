5ea7ad74ce8c9b7d4d07d4bd62a94f6abd2dcd03
strixqlater <strixqlater@gmail.com>
2020-06-03 23:53:31 +0900

    complete UploadFileUtil

--- KM_AF_SHOP/src/main/java/me/cloverclub/util/UploadFileUtil.java
+++ KM_AF_SHOP/src/main/java/me/cloverclub/util/UploadFileUtil.java
@@ -1,9 +1,13 @@
 package me.cloverclub.util;
 
 import java.io.File;
+import java.text.DecimalFormat;
+import java.util.Calendar;
 import java.util.UUID;
 
 import org.springframework.util.FileCopyUtils;
+
+import net.coobird.thumbnailator.Thumbnails;
 
 public class UploadFileUtil {
 	static final int THUMB_HEIGHT = 300;
@@ -24,7 +28,32 @@ public class UploadFileUtil {
 		File thumbnail = new File(imgPath + File.separator + "s" + File.separator + thumbFileName);
 		
 		if(image.exists()) {
+			thumbnail.getParentFile().mkdirs();
+			Thumbnails.of(image).size(THUMB_WIDTH, THUMB_HEIGHT).toFile(thumbnail);
+		}
+		
+		return newFileName;
+	}
+	
+	public static String calcPath(String uploadPath) {
+		Calendar calendar = Calendar.getInstance();
+		String yearPath = File.separator + calendar.get(Calendar.YEAR);
+		String monthPath = yearPath + File.separator + new DecimalFormat("00").format(calendar.get(Calendar.MONTH) + 1);
+		String datePath = monthPath + File.separator + new DecimalFormat("00").format(calendar.get(Calendar.DATE));
+		
+		makeDir(uploadPath, yearPath, monthPath, datePath);
+		makeDir(uploadPath, yearPath, monthPath, datePath + "\\s");
+
+		return datePath;
+	}
+	
+	private static void makeDir(String uploadPath, String... paths) {
+		if(new File(paths[paths.length - 1]).exists()) return;
+		
+		for(String path: paths) {
+			File dirPath = new File(uploadPath + path);
 			
+			if(!dirPath.exists()) dirPath.mkdir();
 		}
 	}
 }
