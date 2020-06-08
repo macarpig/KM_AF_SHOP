5594a44d82f7ba4ccf35ad0c974d9955cfdd4195
strixqlater <strixqlater@gmail.com>
2020-06-08 17:47:55 +0900

    update ShopVO

--- KM_AF_SHOP/src/main/java/me/cloverclub/vo/ShopVO.java
+++ KM_AF_SHOP/src/main/java/me/cloverclub/vo/ShopVO.java
@@ -6,13 +6,14 @@ import lombok.Data;
 
 @Data
 public class ShopVO {
-
 	private String gdsCode;
 	private String gdsName;
 	private String cateCode;
+	private String cateCodeRef;
 	private int gdsPrice;
 	private String gdsDesc;
 	private String gdsImg;
+	private String gdsThumbImg;
 	private Date gdsDate;
 	private int gdsView;
 }
