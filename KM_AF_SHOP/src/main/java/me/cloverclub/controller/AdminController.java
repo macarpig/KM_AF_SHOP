f57744c65f161e3480614d12e3845037330cadb4
strixqlater <strixqlater@gmail.com>
2020-06-08 17:58:18 +0900

    update AdminController (due to Image Upload)

--- KM_AF_SHOP/src/main/java/me/cloverclub/controller/AdminController.java
+++ KM_AF_SHOP/src/main/java/me/cloverclub/controller/AdminController.java
@@ -1,17 +1,22 @@
 package me.cloverclub.controller;
 
+import java.io.File;
 import java.util.List;
+
+import javax.annotation.Resource;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.multipart.MultipartFile;
 
 import lombok.AllArgsConstructor;
 import lombok.extern.log4j.Log4j;
 import me.cloverclub.service.AdminService;
 import me.cloverclub.service.CategoryService;
+import me.cloverclub.util.UploadFileUtil;
 import me.cloverclub.vo.CategoryVO;
 import me.cloverclub.vo.GoodsVO;
 import net.sf.json.JSONArray;
@@ -23,6 +28,9 @@ import net.sf.json.JSONArray;
 public class AdminController {
 	private AdminService a_service;
 	private CategoryService c_service;
+	
+	@Resource(name = "uploadPath")
+	private String uploadPath;
 	
 	// get admin index
 	@GetMapping("/index")
@@ -46,7 +54,23 @@ public class AdminController {
 	
 	// post goods add
 	@PostMapping("/goods/add")
-	public String postGoodsAdd(GoodsVO vo) throws Exception {
+	public String postGoodsAdd(GoodsVO vo, MultipartFile file) throws Exception {
+		String imgUploadPath = uploadPath + File.separator + "imgUpload";
+		String ymdPath = UploadFileUtil.calcPath(imgUploadPath);
+		String fileName = null;
+		
+		if(file != null) {
+			fileName = UploadFileUtil.fileUpload(imgUploadPath, file.getOriginalFilename(), file.getBytes(), ymdPath);
+		}
+		
+		else {
+			fileName = uploadPath + File.separator + "images" + File.separator + "none.png";
+		}
+		
+		vo.setGdsImg(File.separator + "imgUpload" + ymdPath + File.separator + fileName);
+		vo.setGdsThumbImg(File.separator + "imgUpload" + ymdPath + File.separator + "s" + File.separator + "s_" + fileName);
+		
+		
 		a_service.goodsAdd(vo);
 		
 		return "redirect:/admin/goods/add";
